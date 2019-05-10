25088662662c6b741dd63e109a51e86dfbeb0cc8
nblomber <niko.blomberg@student.oulu.fi>
2019-05-10 14:21:37 +0300

    Finding files from Path and saving them into a dictionary. Seems to work fine for now

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+import os
+Path = "C:\\Users\\nblomber\\PycharmProjects\\MetaGen\\testi"
 
+dict = {}
+headers = "asd7;asd2:asd89"
 
-with open("test.csv", encoding="utf8") as reader:
-    lines = reader.read()
-    print(lines)
-    print(1-10)
+for root, dirs, files in os.walk(Path):
+    for f in files:
+        if '.csv' in f:
+            dict[os.path.join(root, f)] = {'filename': f, 'headers': headers}
+
+for f in dict.keys():
+    print("Path: " + f + "\nFilename: " + dict[f].get('filename') + "\nHeaders: " + dict[f].get('headers'))
+
+# import os
+
+# for file in os.listdir(Path):
+#     if file.endswith(".csv"):
+#         print(os.path.join(Path, file))
+#
+
+# GLOB
+# import glob
+# Path = "C:\\Users\\nblomber\\PycharmProjects\\MetaGen\\testi"
+#
+#
+# files = [f for f in glob.glob(Path, recursive=True)]
+# print(files)
