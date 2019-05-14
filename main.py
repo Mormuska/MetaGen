b5ee69a39619a631040c0c79a0264c6830fee70e
Niko Blomberg <Niko6141@gmail.com>
2019-05-14 10:06:30 +0300

    Finished file saving Now loader saves all file names and paths. Headers and types only load if name_ext is a match. Fixed a bug with headers getting TypeErrors

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from utils import *
 #Path = "C:\\Users\\nblomber\\PycharmProjects\\MetaGen\\testi"
-Path = "C:\\Users\\Niko\\PycharmProjects\\MetaGen\\testi"
+Path = "D:\\Data\\V4InputV4.0.2-UNSW"
 
+#Logger and try loop for files
+name_ext = '.csv'
 regex = ','
-meta_dict = meta_loader(Path, '.csv')
+meta_dict = meta_loader(Path)
 
-meta_dict = load_headers(meta_dict, regex)
-meta_dict = load_types(meta_dict, regex)
+meta_dict = load_headers(meta_dict, regex, name_ext)
+meta_dict = load_types(meta_dict, regex, name_ext)
 
 for f in meta_dict.keys():
     print("\nPath: " + f + "\nFilename: " + meta_dict[f].get('filename') + "\nHeaders: ", end="")
     print(meta_dict[f].get('headers'))
     print("Data types: ", end="")
     print(meta_dict[f].get('types'), end="\n")
-#meta_save(meta_dict, Path)
+print(len(meta_dict.keys()))
+meta_save(meta_dict, "D:\\Data")
--- utils.py
+++ utils.py
@@ -1,59 +1,72 @@
 import os, ast
 
 
-def meta_loader(absolute_path, name_ext):
+def meta_loader(absolute_path):
     """
     Finds all files with name_ext (e.q '.csv') in the chosen input folder and subfolders under it. Loads headers from
     the file and saves them, filename and file location into a meta_dict dictionary where file location is the KEY.
 
     :param absolute_path: Absolute path to input  (String)
-    :param name_ext: Filename extension (String)
     :return meta_dict: Metadata stored in a dictionary. PATH to a file is the KEY and it includes HEADERS and FILENAME
     """
     meta_dict = {}
     for root, dirs, files in os.walk(absolute_path):
         for f in files:
-            if name_ext in f:
-                meta_dict[os.path.join(root, f)] = {'filename': f}
+            meta_dict[os.path.join(root, f)] = {'filename': f}
+
     return meta_dict
 
 
-def load_headers(meta_dict, regex):
+def load_headers(meta_dict, regex, name_ext):
 
     for path in meta_dict:
-        file_obj = open(path, 'r')
-        headers = file_obj.readline().rstrip().split(regex)
-        meta_dict[path].update({'headers': headers})
-        file_obj.close()
+        if path.endswith(name_ext):
+            file_obj = open(path, 'r')
+            headers = file_obj.readline().rstrip().split(regex)
+            meta_dict[path].update({'headers': headers})
+            file_obj.close()
     return meta_dict
 
 
-def load_types(meta_dict, regex):
+def load_types(meta_dict, regex, name_ext):
 
     for path in meta_dict:
-        types = []
-        file_obj = open(path, 'r')
-        next(file_obj)   # Skipping headers
-        first_line = file_obj.readline().rstrip().split(regex)
-        for member in first_line:
-            try:
-                types.append(type(ast.literal_eval(member)).__name__)
-            except ValueError:
-                types.append("Unknown")
-                # Prompt Logger
-        meta_dict[path].update({'types': types})
-        file_obj.close()
+        if path.endswith(name_ext):
+            types = []
+            file_obj = open(path, 'r')
+            next(file_obj)   # Skipping headers
+            first_line = file_obj.readline().rstrip().split(regex)
+            for member in first_line:
+                try:
+                    try:
+                        types.append(type(ast.literal_eval(member)).__name__)
+                    except SyntaxError:
+                        types.append("String")
+                except ValueError:
+                    types.append("Unknown")
+                    # Prompt Logger
+            meta_dict[path].update({'types': types})
+            file_obj.close()
     return meta_dict
 
-#
-# def meta_save(meta_dict, absolute_path, name="\Meta_data.csv", append=False):
-#     path = absolute_path + name
-#     if append:
-#         file_obj = open(path, 'a')
-#     else:
-#         file_obj = open(path, 'w')
-#     file_obj.write('asd;absolute_path')
-#
-#     file_obj.close()
-#     print("Metadata saved into %s", path)
-#     return
+
+def meta_save(meta_dict, absolute_path, name="\Meta_data.csv", append=False):
+    path = absolute_path + name
+    if append:
+        file_obj = open(path, 'a')
+    else:
+        file_obj = open(path, 'w')
+    file_obj.write("Metadata list\n")
+    file_obj.write('Listed files: %d' % len(meta_dict.keys()))
+    file_obj.write('\n')
+    for path in meta_dict:
+        file_obj.write('\n')
+        file_obj.write('Path;Name;Headers;\n')
+        try:
+            file_obj.write('%s;%s;%s\n' % (path, meta_dict[path].get('filename'), (';'.join(meta_dict[path].get('headers')))))
+            file_obj.write(';;%s\n' % (';'.join(meta_dict[path].get('types'))))
+        except TypeError:
+            file_obj.write('%s;%s;None\n' % (path, meta_dict[path].get('filename')))
+    file_obj.close()
+    print("Metadata saved into %s", path)
+    return
